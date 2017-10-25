292224c44fd6eea4450966ad1b3db80c50587ac1
Kirill Morozov <13591599+kamorozov@users.noreply.github.com>
2017-10-25 08:05:35 +0300

    Add files via upload
    25.10.2017

--- Rmin + Nmib bin DD.py
+++ Rmin + Nmib bin DD.py
@@ -38,16 +38,16 @@ def ch_d_to_c(number):
     return answer
 
 
-#mix = input('смесь: ')
-mix = 'bt'
+mix = input('смесь (bt, te, ea): ')
+# mix = 'bt'
 x_1_temp = input('количество первого компонента в питании(доли): ')
 x_1 = ch_comma_to_dot(x_1_temp)
-#x_1d_temp = input('количество первого компонента в дистиллате(доли): ')
-#x_1d = ch_comma_to_dot(x_1d_temp)
-x_1d = 0.999
-#F_temp = input('питание кмоль/ч: ')
-#F = ch_comma_to_dot(F_temp)
-F = 0.4
+x_1d_temp = input('количество первого компонента в дистиллате(доли): ')
+x_1d = ch_comma_to_dot(x_1d_temp)
+# x_1d = 0.999
+F_temp = input('питание кмоль/ч: ')
+F = ch_comma_to_dot(F_temp)
+# F = 0.4
 x_1w = 1 - x_1d
 D = F * (x_1 - x_1w)/(x_1d - x_1w)
 f = F / D
@@ -56,26 +56,59 @@ Rmin = (x_1d - y_eq(x_1)) / (y_eq(x_1) - x_1)
 R = Rmin * 1.3
 y = x_1d
 x = x_1d
+x_work = [x]
+y_work = [y]
 N = 0
 x_pr = 0
 x_last = 0
 while x > x_1w:
     x = x_eq(y)
+    x_work.append(x)
+    y_work.append(y_eq(x))
     x_last = x
     if round(x_last, 5) == round(x_pr, 5):
         break
     if x > x_1:
         y_work_up = R / (R + 1) * x + x_1d / (R + 1)
         y = y_work_up
+        x_work.append(x)
+        y_work.append(y)
     elif x < x_1:
         y_work_down = (R + f) / (R + 1) * x - (1 - f) / (R + 1) * x_1w
         y = y_work_down
+        x_work.append(x)
+        y_work.append(y)
     x_pr = x_last
     N +=1
 
-print(ch_d_to_c(y_eq(x_1) / x_1))
-print(ch_d_to_c(y_eq(x_1)))
-print(ch_d_to_c(Rmin))
-print(x_eq(0.999))
-print(N)
+plt.plot(x_work, y_work)
+#print(*(round(x, 4) for x in x_work))
+#print(*(round(y, 4) for y in y_work))
+print('Rmin =', ch_d_to_c(Rmin))
+print('число тарело:', N)
+
+x = []
+y =[]
+for i in range(0, 101):
+    x.append(i / 100)
+    y_i = y_eq(i / 100)
+    y.append(y_i)
+plt.plot([0, 1], [0, 1])
+plt.plot(x, y)
+
+x_work_line = []
+y_work_line = []
+for j in range(int(x_1w * 1000), int(x_1d * 1000 + 1)):
+    x_work_line.append(j / 1000)
+    if j / 1000 <= x_1:
+        y_work_down = (R + f) / (R + 1) * (j / 1000) - (1 - f) / (R + 1) * x_1w
+        y_work_line.append(y_work_down)
+    elif j / 1000 >= x_1:
+        y_work_up = R / (R + 1) * (j / 1000) + x_1d / (R + 1)
+        y_work_line.append(y_work_up)
+
+plt.plot(x_work_line, y_work_line)
+plt.axis([0, 1, 0, 1])
+
+plt.show()
 
